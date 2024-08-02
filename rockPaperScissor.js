a9bb29f8f4cd3c3b725991e18694a4d2b16a104b
H.U.B. <kunstkr1tik@web.de>
2024-08-02 23:30:57 +0200

    add human choice function

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -6,3 +6,26 @@ function getComputerChoice() {
     let choiceIndex = Math.floor(Math.random() * CHOICES.length);
     return CHOICES[choiceIndex];
 }
+
+function isValidRange(input) {
+    return input >= 1 && input <= CHOICES.length;
+}
+
+function getHumanChoice() {
+    let choiceIndex = -1;
+    while (true) {
+        let input = prompt("Type 1 for rock\nType 2 for paper\nType 3 for scissors")
+        try {
+            choiceIndex = parseInt(input);
+
+            if (isValidRange(choiceIndex)) {
+                break;
+            }
+            throw new RangeError(`Illegal range. Must be between 1 and ${CHOICES.length}`);
+        } catch (error) {
+            console.log(`Invalid input: ${input}\nPlease try again!`)
+            choiceIndex = -1;
+        }
+    } 
+    return CHOICES[choiceIndex-1];
+}
