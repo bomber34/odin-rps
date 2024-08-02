08b759cb9cd275b8a0f9c058ffd063ef98e2d52d
H.U.B. <kunstkr1tik@web.de>
2024-08-02 23:08:16 +0200

    add computer choice method

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,1 +1,8 @@
 console.log("This is a simple Rock Paper Scissors game");
+
+const CHOICES = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let choiceIndex = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[choiceIndex];
+}
